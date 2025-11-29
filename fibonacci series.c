07b514c48904c5317011597562e7c59b8c6ec01a
Haripriya558 <haripriyadasari2006@gmail.com>
2025-11-29 11:18:14 +0530

    Add files via upload

---
+++ fibonacci series.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n,first=0,second=1,next;
+printf("enter no.of terms:");
+scanf("%d",&n);
+printf("fibonacci series:");
+for(int i=0;i<n;i++)
+{
+printf("%d",first);
+next=first+second;
+first=second;
+second=next;
+}
+return 0;
+}
